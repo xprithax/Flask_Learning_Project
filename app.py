d6b12d13f2ecafe77e0033c676dadec77e78cefa
Pritha <96653821+xprithax@users.noreply.github.com>
2024-04-21 03:56:32 +0000

    added doctor.html, added json api

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 
 app = Flask(__name__)
+
+Doctors=[
+  {
+    'id':'1',
+    'name':'Dr. Amit',
+    'speciality':'Cardiology',
+    'location':'Delhi',
+    'rating':'4.5',
+  },
+  {
+    'id':'2',
+    'name':'Dr. Karan',
+    'speciality':'Dentist',
+    'location':'Delhi',
+    'rating':'4.5',
+  },
+  {
+    'id':'3',
+    'name':'Dr. Shalini',
+    'speciality':'Gynecology',
+    'location':'Delhi',
+    'rating':'4.5',
+  },
+  {
+    'id':'4',
+    'name':'Dr. Saakshi',
+    'speciality':'Opthalmology',
+    'location':'Delhi',
+    'rating':'4.5',
+  }
+]
 
 
 @app.route("/")
 def hello():
-  return render_template("home.html")
+  return render_template("home.html", doc=Doctors)
 
+@app.route("/api/doc")
+def doc_list():
+  return jsonify(Doctors)
 
 if (__name__ == "__main__"):
   app.run(host="0.0.0.0", debug=True)
